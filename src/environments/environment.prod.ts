3d9767b7a902b61be9be2a35af82071b9081dadd
Ferny79 <fernando.dotlj@gmail.com>
2025-12-01 00:58:41 -0500

    providenoop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true, // Esto le dice a Angular que es el build final
+    // Aquí usamos la URL que ya subiste a Render
+    base: 'https://ecobackend-ubbd.onrender.com' 
+};
