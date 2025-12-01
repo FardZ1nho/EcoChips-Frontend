bdb23af6690ccd3e0aee47d45082c72895815862
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2025-12-01 10:57:53 -0500

    Final: Conexión Frontend-Backend completa con URL de Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment={
-    production:false,
-    base:'http://localhost:8080'
-}
+export const environment = {
+  production: true, 
+  // Usa la URL de Render para la producción
+  base: 'https://ecobackend-ubbd.onrender.com/api/v1/' 
+};
