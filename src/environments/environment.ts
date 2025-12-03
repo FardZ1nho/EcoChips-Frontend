944d6ca02b8e229c744c3f046c4a26e2e993655f
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2025-12-03 00:30:07 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true, 
-  base: 'https://ecobackend-ubbd.onrender.com/', 
+  base: 'https://ecobackend-1zas.onrender.com', 
   openWeatherApiKey: '439388cfcbce8301670fadc0e69a5e15'
 };
 
